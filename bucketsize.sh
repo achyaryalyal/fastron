f7fa0876ceb17a01350526044de891d2315ff80d
achyaryalyal <aymarxp@gmail.com>
2021-04-07 11:27:13 +0700

    Delete bucketsize.sh

--- bucketsize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bucket_size="$(s5cmd --endpoint-url http://s3-id-jkt-1.kilatstorage.id/ du --humanize s3://bbg-master-cabinet/* && s5cmd --endpoint-url http://s3-id-jkt-1.kilatstorage.id/ du --humanize s3://bbg-development-cabinet/*)"
-
-echo -e "$bucket_size"
