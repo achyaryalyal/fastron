ee42df5c15455fd2f6cec3b8a950ca17663bf690
achyaryalyal <aymarxp@gmail.com>
2022-12-23 10:11:22 +0700

    Update script-moodle-delete-old-files.sh

--- moodle/script-moodle-delete-old-files.sh
+++ moodle/script-moodle-delete-old-files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# change [YOUR_MOODLE_PATH] with your moodle wwwroot path
 
+# change [YOUR_MOODLE_PATH] with your moodle wwwroot path
 old_files=($(/usr/bin/php8.1 /[YOUR_MOODLE_PATH]/admin/script-moodle-list-old-files.php))
 for i in "${old_files[@]}"; do [ -f "$i" ] && rm "$i" && echo "$i deleted successfully"; done
