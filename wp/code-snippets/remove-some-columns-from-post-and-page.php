3cc768dea029fd8a8722b4a0400d48214fbbadbb
achyaryalyal <aymarxp@gmail.com>
2023-04-23 14:42:03 +0700

    Update remove-some-columns-from-post-and-page.php

--- wp/code-snippets/remove-some-columns-from-post-and-page.php
+++ wp/code-snippets/remove-some-columns-from-post-and-page.php
@@ -2,17 +2,17 @@
 // Remove Some Columns from Post and Page
 /////////////////////////////////
 
-function my_columns_filter( $columns ) {
+function my_columns_filter($columns) {
     unset($columns['tags']);
     unset($columns['comments']);
-	unset($columns['wpseo-score']);
-	unset($columns['wpseo-score-readability']);
-	unset($columns['wpseo-title']);
-	unset($columns['wpseo-metadesc']);
-	unset($columns['wpseo-focuskw']);
-	unset($columns['wpseo-links']);
-	unset($columns['wpseo-linked']);
+    unset($columns['wpseo-score']);
+    unset($columns['wpseo-score-readability']);
+    unset($columns['wpseo-title']);
+    unset($columns['wpseo-metadesc']);
+    unset($columns['wpseo-focuskw']);
+    unset($columns['wpseo-links']);
+    unset($columns['wpseo-linked']);
     return $columns;
 }
-add_filter( 'manage_edit-post_columns', 'my_columns_filter', 10, 1 );
-add_filter( 'manage_edit-page_columns', 'my_columns_filter', 10, 1 );
+add_filter('manage_edit-post_columns', 'my_columns_filter', 10, 1);
+add_filter('manage_edit-page_columns', 'my_columns_filter', 10, 1);
