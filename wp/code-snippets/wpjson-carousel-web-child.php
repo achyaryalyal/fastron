155b338e3c88efd1bcb694d9967cece827c2cb30
achyaryalyal <aymarxp@gmail.com>
2023-05-01 16:19:12 +0700

    Update wpjson-carousel-web-child.php

--- wp/code-snippets/wpjson-carousel-web-child.php
+++ wp/code-snippets/wpjson-carousel-web-child.php
@@ -9,11 +9,11 @@ function wpjc() {
 	$domain_parent = 'bbg.ac.id'; // SET UP HERE
 	
 	$items = $limit;
-	$url_1 = "https://'.$domain_parent.'/wp-json/wp/v2/posts?tags=".$id_tag_child."&per_page=".$items."&_embed";
+	$url_1 = 'https://'.$domain_parent.'/wp-json/wp/v2/posts?tags='.$id_tag_child.'&per_page='.$items.'&_embed';
 	$json_1 = file_get_contents($url_1);
 	$arr_1 = json_decode($json_1, FALSE);
 	$id_category_announcement_child = 1;
-	$url_2 = "https://'.$subdomain_child.'.'.$domain_parent.'/wp-json/wp/v2/posts?categories=".$id_category_announcement_child."&per_page=".$items."&_embed";
+	$url_2 = 'https://'.$subdomain_child.'.'.$domain_parent.'/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items.'&_embed';
 	$json_2 = file_get_contents($url_2);
 	$arr_2 = json_decode($json_2, FALSE);
 	$total_items = $items * 2;
