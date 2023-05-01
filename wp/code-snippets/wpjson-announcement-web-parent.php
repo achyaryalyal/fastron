a14dc0848c77cdd517b745060cc9d5e200ce5fd8
achyaryalyal <aymarxp@gmail.com>
2023-05-01 13:27:30 +0700

    Update wpjson-announcement-web-parent.php

--- wp/code-snippets/wpjson-announcement-web-parent.php
+++ wp/code-snippets/wpjson-announcement-web-parent.php
@@ -2,46 +2,39 @@
 // WPJSON Announcement Web Parent
 ////////////////////////////////////////////
 
-function wpjan($atts = [], $content = null, $tag = '') {
-	// normalize attribute keys, lowercase
-	$atts = array_change_key_case((array) $atts, CASE_LOWER);
+function wpjan() {
+	$limit = 7; // SET UP HERE
+	$domain_parent = 'bbg.ac.id'; // SET UP HERE
 	
-	// override default attributes with user attributes
-	$my_atts = shortcode_atts(
-		array(
-			'limit' => 'WordPress.org',
-		), $atts, $tag
-	);
-	$limit = esc_html__($my_atts['limit'], 'wpjson_announcement');
-	$limit = str_replace(' ', '', $limit);
-	
-	$items = 3; // set limit HERE
+	$items = 3; // set UP HERE
 	$id_category_announcement_parent = 70;
 	$id_category_announcement_child = 1;
-	
-	$url_1 = 'https://bbg.ac.id/wp-json/wp/v2/posts?categories='.$id_category_announcement_parent.'&per_page='.$items;
+	$url_1 = 'https://'.$domain_parent.'/wp-json/wp/v2/posts?categories='.$id_category_announcement_parent.'&per_page='.$items;
 	$json_1 = file_get_contents($url_1);
 	$arr_1 = json_decode($json_1, FALSE);
 	
-	$url_2 = 'https://baa.bbg.ac.id/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
+	$subdomain_child = 'baa'; // SET UP HERE
+	$url_2 = 'https://'.$subdomain_child.'.'.$domain_parent.'/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
 	$json_2 = file_get_contents($url_2);
 	$arr_2 = json_decode($json_2, FALSE);
 	
-	$url_3 = 'https://buk.bbg.ac.id/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
+	$subdomain_child = 'buk'; // SET UP HERE
+	$url_3 = 'https://'.$subdomain_child.'.'.$domain_parent.'/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
 	$json_3 = file_get_contents($url_3);
 	$arr_3 = json_decode($json_3, FALSE);
 	
-	$url_4 = 'https://btik.bbg.ac.id/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
+	$subdomain_child = 'btik'; // SET UP HERE
+	$url_4 = 'https://'.$subdomain_child.'.'.$domain_parent.'/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
 	$json_4 = file_get_contents($url_4);
 	$arr_4 = json_decode($json_4, FALSE);
 	
-	$url_5 = 'https://birmas.bbg.ac.id/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
+	$subdomain_child = 'birmas'; // SET UP HERE
+	$url_5 = 'https://'.$subdomain_child.'.'.$domain_parent.'/wp-json/wp/v2/posts?categories='.$id_category_announcement_child.'&per_page='.$items;
 	$json_5 = file_get_contents($url_5);
 	$arr_5 = json_decode($json_5, FALSE);
 	
 	$total_items = $items * 5;
 	$arr = array_merge($arr_1, $arr_2, $arr_3, $arr_4, $arr_5);
-	
 	$arr_sort_date = array_column($arr, 'date');
 	array_multisort($arr_sort_date, SORT_DESC, $arr);
 	
@@ -56,7 +49,7 @@ function wpjan($atts = [], $content = null, $tag = '') {
 			$subdomain = parse_url($link); $subdomain = $subdomain['host'];
 			$date = $arr[$i]->date;
 			$date_converted = date("j F Y", strtotime($date));
-			$date_converted = wpjcan_konversi_tanggal("j F Y", $date_converted, $bahasa="id");
+			$date_converted = wpjan_konversi_tanggal("j F Y", $date_converted, $bahasa="id");
 			$result .= '<li class="list_pengumuman"><h3 class="font-osc" style="color:#337ab7;font-size:14px;font-weight:600;line-height:18px;margin:0;padding:4px 0;"><a href="'.$link.'" target="_blank">'.$title.'</a></h3><p class="post-meta" style="font-family:Arimo,sans-serif;font-size:10px;margin:1px;padding:0;"><span class="tie-date" style="color:#444;font-size:11px;"><i class="fa fa-calendar-alt"></i> '.$date_converted.' <i class="fa fa-id-card-alt" style="padding-left:6px;"></i> '.$subdomain.'</span></p></li>';
 		}
 	}
@@ -86,9 +79,9 @@ function wpjan($atts = [], $content = null, $tag = '') {
 </style>';
 	return $result;
 } 
-add_shortcode('wpjson_announcement', 'wpjan'); // [wpjson_announcement limit="5"]
+add_shortcode('wpjson_announcement', 'wpjan');
 
-function wpjcan_konversi_tanggal($format, $tanggal="now", $bahasa="id") {
+function wpjan_konversi_tanggal($format, $tanggal="now", $bahasa="id") {
 	$en = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","January","February","March","April","May","June","July","August","September","October","November","December");
 	$en_singkat = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
 	$id = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
