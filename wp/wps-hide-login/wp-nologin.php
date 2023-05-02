4b64d11a8b870e82cbe4b08435733584fcc0393e
achyaryalyal <aymarxp@gmail.com>
2023-05-02 12:28:34 +0700

    Create wp-nologin.php

---
+++ wp/wps-hide-login/wp-nologin.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Log Masuk &lsaquo; Universitas Bina Bangsa Getsempena &#8212; WordPress</title>
+  <meta name='robots' content='noindex, follow' />
+  <!--n2css--><link rel='stylesheet' id='dashicons-css' href='/wp-includes/css/dashicons.min.css?ver=6.2' type='text/css' media='all' />
+  <link rel='stylesheet' id='buttons-css' href='/wp-includes/css/buttons.min.css?ver=6.2' type='text/css' media='all' />
+  <link rel='stylesheet' id='forms-css' href='/wp-admin/css/forms.min.css?ver=6.2' type='text/css' media='all' />
+  <link rel='stylesheet' id='l10n-css' href='/wp-admin/css/l10n.min.css?ver=6.2' type='text/css' media='all' />
+  <link rel='stylesheet' id='login-css' href='/wp-admin/css/login.min.css?ver=6.2' type='text/css' media='all' />
+  <link rel='stylesheet' id='logincust_styles-css' href='/wp-content/plugins/login-customizer/src/Customizer/Panel/Assets/CSS/customizer.css?ver=6.2' type='text/css' media='all' />
+  <style id='logincust_styles-inline-css' type='text/css'>
+  body.login {background-color: #0d003e;}body.login div#login h1 a {background-image: url(" /wp-content/uploads/2021/05/ubbg.png ");}#login form#loginform, #login form#registerform, #login form#lostpasswordform {padding: 26px 24px 46px 25px;border-radius: 10px;box-shadow: 0 1px 50px rgba(2,2,2,0.3);}#login form .forgetmenot {display: none;}#login form#loginform .input, #login form#registerform .input, #login form#lostpasswordform .input {border-width: 0px;box-shadow: unset;}#login form#loginform label, #login form#registerform label, #login form#lostpasswordform label {}#login form#loginform .forgetmenot label, #login form#registerform .forgetmenot label, #login form#lostpasswordform .forgetmenot label {}#login form .submit .button {height: auto;background-color: #e91e63;width: 100%;height: 40px;padding: 0px 12px 2px 12px;border-width: 0px;text-shadow: 0 -1px 1px #e91e63,1px 0 1px #e91e63,0 1px 1px #e91e63,-1px 0 1px #e91e63;}#login form .submit .button:hover, #login form .submit .button:focus {background-color: #e8004d;}.login #nav, .login #nav a, .login #backtoblog a, .login .privacy-policy-page-link a {color: #ffffff;}.login #backtoblog a:hover, .login #nav a:hover, .login .privacy-policy-page-link a:hover {color: #dddddd;}/* Custom CSS for Material Template */
+  #login form#loginform .input,
+  #login form#registerform .input,
+  #login form#lostpasswordform .input {
+      border-bottom: 1px solid #d2d2d2;
+  }
+  .bar {
+      position: relative;
+      display: block;
+      width: 100%;
+  }
+  .bar:before, .bar:after {
+      content: "";
+      height: 2px; 
+      width: 0;
+      bottom: 15px; 
+      position: absolute;
+      background: #e91e63; 
+      transition: all 0.2s ease;
+  }
+  .bar:before { left: 50%; }
+  .bar:after { right: 50%; }
+  input:focus ~ .bar:before, input:focus ~ .bar:after { width: 50%; }
+  </style>
+  <meta name='referrer' content='strict-origin-when-cross-origin' />
+  <meta name="viewport" content="width=device-width" />
+</head>
+<body class="login no-js login-action-login wp-core-ui  locale-id-id">
+	<div id="login">
+		<h1><a href="/">Universitas Bina Bangsa Getsempena</a></h1>
+		<form id="loginform">
+			Untuk alasan keamanan, alamat login situs ini telah kami ubah. <br><br> Bila Anda ingin login, silakan hubungi BTIK UBBG untuk informasi lebih lanjut.
+		</form>		
+	</div>
+	<div class="clear"></div>
+	</body>
+</html>
