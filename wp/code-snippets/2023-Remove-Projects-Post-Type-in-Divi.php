a5ba380d69d8bd09cf36bd6512903e4679017ee4
achyaryalyal <aymarxp@gmail.com>
2023-12-20 10:45:21 +0700

    Create 2023-Remove-Projects-Post-Type-in-Divi.php

---
+++ wp/code-snippets/2023-Remove-Projects-Post-Type-in-Divi.php
@@ -0,0 +1,13 @@
+function remove_divi_projects() {
+	unregister_post_type('project');
+}
+add_action('init', 'remove_divi_projects');
+
+if(!function_exists('cliff_remove_divi_project_post_type')) {
+	function cliff_remove_divi_project_post_type() {
+		unregister_post_type('project');
+		unregister_taxonomy('project_category');
+		unregister_taxonomy('project_tag');
+	}
+}
+add_action('init', 'cliff_remove_divi_project_post_type');
