26ced83a2a937a196961d74ed5239246578f3c74
achyaryalyal <aymarxp@gmail.com>
2023-12-20 10:44:11 +0700

    Create 2023-Remove-Some-Columns-from-Post-and-Page.php

---
+++ wp/code-snippets/2023-Remove-Some-Columns-from-Post-and-Page.php
@@ -0,0 +1,18 @@
+/////////////////////////////////
+// Remove Some Columns from The Post List
+/////////////////////////////////
+
+function my_columns_filter( $columns ) {
+    //unset($columns['tags']);
+    unset($columns['comments']);
+	unset($columns['wpseo-score']);
+	unset($columns['wpseo-score-readability']);
+	unset($columns['wpseo-title']);
+	unset($columns['wpseo-metadesc']);
+	unset($columns['wpseo-focuskw']);
+	unset($columns['wpseo-links']);
+	unset($columns['wpseo-linked']);
+    return $columns;
+}
+add_filter( 'manage_edit-post_columns', 'my_columns_filter', 10, 1 );
+add_filter( 'manage_edit-page_columns', 'my_columns_filter', 10, 1 );
