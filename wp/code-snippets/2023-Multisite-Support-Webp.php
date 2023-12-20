7b79fcef0c288239e93af687e60f25ce587a0393
achyaryalyal <aymarxp@gmail.com>
2023-12-20 10:49:36 +0700

    Create 2023-Multisite-Support-Webp.php

---
+++ wp/code-snippets/2023-Multisite-Support-Webp.php
@@ -0,0 +1,11 @@
+add_filter('site_option_upload_filetypes',
+  function ( $filetypes ) {
+    $filetypes = explode( ' ', $filetypes );
+    if ( ! in_array( 'webp', $filetypes, true ) ) {
+      $filetypes[] = 'webp';
+      $filetypes   = implode( ' ', $filetypes );
+    }
+ 
+    return $filetypes;
+  }
+);
