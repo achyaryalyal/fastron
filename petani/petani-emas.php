b5e3b1416b5422e080f306867c98d2006e63c50f
achyaryalyal <aymarxp@gmail.com>
2024-06-17 20:45:35 +0700

    Update petani-emas.php

--- petani/petani-emas.php
+++ petani/petani-emas.php
@@ -1,8 +1,11 @@
 <?php
 
+sleep(2);
+
 session_start();
 
 $nm_company = 'Petani Emas';
+$nm_website = 'petaniemas.id';
 
 $arr_akun = array(
     array("Chaizir", 45885771),
@@ -33,173 +36,55 @@ if(isset($_GET['logout'])) {
 
 echo '<!doctype html>
     <html lang="en" data-bs-theme="auto">
-    <head><script src="https://getbootstrap.com/docs/5.3/assets/js/color-modes.js"></script>
-
+    <head>
+    <script src="https://getbootstrap.com/docs/5.3/assets/js/color-modes.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    
     <title>Laporan Mingguan '.$nm_company.'</title>
-    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
     <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <!-- Favicons -->
     <link rel="icon" href="petani-emas.png">
-    
     <meta name="theme-color" content="#712cf9">
-
     <style>
+    body {
+        background: linear-gradient(-45deg, #1D1D1F, #3B3130, #B16C04);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+    }
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
     .gold {
-      font-size: 5vw;
-      text-transform: uppercase;
-      line-height:1;
-      text-align: center;
-      background: linear-gradient(90deg, rgba(186,148,62,1) 0%, rgba(236,172,32,1) 20%, rgba(186,148,62,1) 39%, rgba(249,244,180,1) 50%, rgba(186,148,62,1) 60%, rgba(236,172,32,1) 80%, rgba(186,148,62,1) 100%);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;	
-      animation: shine 3s infinite;
-      background-size: 200%;
-      background-position: left;
-    
+        font-size: 40px;
+        text-transform: uppercase;
+        line-height:1;
+        text-align: center;
+        background: linear-gradient(90deg, rgba(186,148,62,1) 0%, rgba(236,172,32,1) 20%, rgba(186,148,62,1) 39%, rgba(249,244,180,1) 50%, rgba(186,148,62,1) 60%, rgba(236,172,32,1) 80%, rgba(186,148,62,1) 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;	
+        animation: shine 3s infinite;
+        background-size: 200%;
+        background-position: left;
     }
     @keyframes shine {
-      to{background-position: right}
+        to{background-position: right}
     }
-    
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
     </style>
     
     <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
-  </head>
-  <body class="bg-body-tertiary">
-    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-      <symbol id="check2" viewBox="0 0 16 16">
-        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-      </symbol>
-      <symbol id="circle-half" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-      </symbol>
-      <symbol id="moon-stars-fill" viewBox="0 0 16 16">
-        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
-        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
-      </symbol>
-      <symbol id="sun-fill" viewBox="0 0 16 16">
-        <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-      </symbol>
-    </svg>
-    
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-      <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-              id="bd-theme"
-              type="button"
-              aria-expanded="false"
-              data-bs-toggle="dropdown"
-              aria-label="Toggle theme (auto)">
-        <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-            Light
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-            Dark
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em"><use href="#circle-half"></use></svg>
-            Auto
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-      </ul>
-    </div>
+</head>
+<body class="bg-body-tertiary">
     
     <div class="container">';
 
@@ -244,15 +129,15 @@ echo '<!doctype html>
           
             <!-- TradingView Widget BEGIN -->
             <div class="tradingview-widget-container">
-              <div class="tradingview-widget-container__widget"></div>
-              <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+              <div class="tradingview-widget-container__widget" style="background:#2B3035;"></div>
+              <div class="tradingview-widget-copyright" style="display:none;"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
               <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
               {
               "symbol": "OANDA:XAUUSD",
-              "width": 300,
+              "width": "100%",
               "isTransparent": false,
               "colorTheme": "dark",
-              "locale": "en"
+              "locale": "id"
             }
               </script>
             </div>
@@ -260,15 +145,15 @@ echo '<!doctype html>
             
             <!-- TradingView Widget BEGIN -->
             <div class="tradingview-widget-container">
-              <div class="tradingview-widget-container__widget"></div>
-              <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+              <div class="tradingview-widget-container__widget" style="background:#2B3035;"></div>
+              <div class="tradingview-widget-copyright" style="display:none;"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
               <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
               {
               "feedMode": "symbol",
               "symbol": "OANDA:XAUUSD",
               "isTransparent": false,
               "displayMode": "regular",
-              "width": "300",
+              "width": "100%",
               "height": "550",
               "colorTheme": "dark",
               "locale": "id"
@@ -279,9 +164,9 @@ echo '<!doctype html>
             
           </div>
           
-          <div class="col-md-7 col-lg-8">
+          <div class="col-md-7 col-lg-8 mb-5">
           
-            <h4 class="mb-3">Informasi</h4>
+            <h4 class="mb-3">Informasi Umum</h4>
             
             <form method="post" action="petani-emas.php">
             
@@ -369,7 +254,7 @@ echo '<!doctype html>
         if(isset($_POST['generate'])) {
             //echo '<pre>'; print_r($_POST); echo '</pre>'; die();
             //echo '<hr>';
-            echo '<div class="row mt-5">';
+            echo '<div id="laporan" class="row mt-5">';
             if($_POST['kondisi']==1) {
                 for($i = 0; $i < $_POST['count']; $i++) {
                     $num = $i + 1;
@@ -400,6 +285,9 @@ _Strategy & Fund Manager_</textarea>
 <script>
 var clipboard_'.$_POST['nomor_'.$num].' = new ClipboardJS("#btn_'.$_POST['nomor_'.$num].'");
 clipboard_'.$_POST['nomor_'.$num].'.on("success", function(e) {
+    document.getElementById("btn_'.$_POST['nomor_'.$num].'").classList.remove("btn-success");
+    document.getElementById("btn_'.$_POST['nomor_'.$num].'").classList.add("btn-outline-secondary");
+    document.getElementById("text_'.$_POST['nomor_'.$num].'").remove();
     console.info("Action:", e.action);
     console.info("Text:", e.text);
     console.info("Trigger:", e.trigger);
@@ -451,6 +339,9 @@ _Strategy & Fund Manager_</textarea>
 <script>
 var clipboard_'.$_POST['nomor_'.$num].' = new ClipboardJS("#btn_'.$_POST['nomor_'.$num].'");
 clipboard_'.$_POST['nomor_'.$num].'.on("success", function(e) {
+    document.getElementById("btn_'.$_POST['nomor_'.$num].'").classList.remove("btn-success");
+    document.getElementById("btn_'.$_POST['nomor_'.$num].'").classList.add("btn-outline-secondary");
+    document.getElementById("text_'.$_POST['nomor_'.$num].'").remove();
     console.info("Action:", e.action);
     console.info("Text:", e.text);
     console.info("Trigger:", e.trigger);
@@ -502,6 +393,9 @@ _Strategy & Fund Manager_</textarea>
 <script>
 var clipboard_'.$_POST['nomor_'.$num].' = new ClipboardJS("#btn_'.$_POST['nomor_'.$num].'");
 clipboard_'.$_POST['nomor_'.$num].'.on("success", function(e) {
+    document.getElementById("btn_'.$_POST['nomor_'.$num].'").classList.remove("btn-success");
+    document.getElementById("btn_'.$_POST['nomor_'.$num].'").classList.add("btn-outline-secondary");
+    document.getElementById("text_'.$_POST['nomor_'.$num].'").remove();
     console.info("Action:", e.action);
     console.info("Text:", e.text);
     console.info("Trigger:", e.trigger);
@@ -525,7 +419,7 @@ clipboard_'.$_POST['nomor_'.$num].'.on("error", function(e) {
             }
             echo '</div>
             <script>
-            document.getElementById("generate").scrollIntoView();
+            document.getElementById("laporan").scrollIntoView({ behavior: "smooth", block: "start" });
             </script>';
         }
         echo '</main>';
@@ -546,7 +440,7 @@ clipboard_'.$_POST['nomor_'.$num].'.on("error", function(e) {
         .form-signin {
           width: 100%;
           max-width: 330px;
-          padding: 15px;
+          padding: 15px 15px 0px 15px;
           margin: auto;
         }
         .form-signin .form-floating:focus-within {
@@ -557,28 +451,46 @@ clipboard_'.$_POST['nomor_'.$num].'.on("error", function(e) {
         }
         </style>
         <main class="form-signin">
-        <form method="post" action="petani-emas.php">
-        <center><img class="mb-4" src="petani-emas.png" alt="" width="72" height="69"></center>
-        <h1 class="h3 gold text-center mb-3 fw-normal">'.$nm_company.'</h1>';
-        if(isset($error_message)) {
-            echo '<div class="alert alert-warning">'.$error_message.'</div>';
-        }
-        echo '<div class="form-floating">
-          <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password" required>
-          <label for="floatingPassword">Password</label>
-        </div>
-        <button class="btn btn-warning w-100 py-2" name="login" type="submit">Login</button>
-      </form>
-      </main>';
+            <form method="post" class="mt-2" action="petani-emas.php">
+            <center><img class="mb-4" src="petani-emas.png" alt="" style=" width: 130px; height: auto; filter: blur(0); -webkit-filter: blur(0); "></center>
+            <h1 class="h3 gold text-center mb-3 fw-normal">'.$nm_company.'</h1>';
+            if(isset($error_message)) {
+                echo '<div class="alert alert-warning">'.$error_message.'</div>';
+            }
+            echo '<div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password" required>
+                <label for="floatingPassword">Password</label>
+            </div>
+            <button class="btn btn-warning w-100 py-2" name="login" type="submit">Login</button>
+            </form>
+            
+            <div class="text-center fw-bold fst-italic mt-4 mb-4"><small>Make XAUUSD <span class="text-warning">“Your Passive Income”</span></small></div>
+            
+            <!-- TradingView Widget BEGIN -->
+            <div class="tradingview-widget-container">
+              <div class="tradingview-widget-container__widget" style="background:#2B3035;"></div>
+              <div class="tradingview-widget-copyright" style="display:none;"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+              <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
+              {
+              "symbol": "OANDA:XAUUSD",
+              "width": "100%",
+              "isTransparent": false,
+              "colorTheme": "dark",
+              "locale": "id"
+              }
+              </script>
+            </div>
+            <!-- TradingView Widget END -->
+            
+        </main>';
     }
     
-    echo '<footer class="my-5 pt-5 text-body-secondary text-center text-small">
-        <p class="mb-4">&copy; '.date('Y').' '.$nm_company.'</p>
-        <div><img src="petani-emas-norton.svg" width="80px"></div>
+    echo '<footer class="text-body-secondary text-center text-small">
+        <p class="mb-3">&copy; '.date('Y').' '.$nm_website.'</p>
+        <p class="mb-3"><img src="petani-emas-norton.svg" width="80px"></p>
       </footer>
     </div>
     <script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://getbootstrap.com/docs/5.3/examples/checkout/checkout.js"></script>
 </body>
 </html>';
 
