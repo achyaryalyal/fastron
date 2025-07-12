af1272437cc4997241fa51610130d473fa6dfc2c
achyaryalyal <aymarxp@gmail.com>
2025-07-12 19:02:26 +0700

    Update cek_ip_access_belum_deny.py

--- nginx/cek_ip_access_belum_deny.py
+++ nginx/cek_ip_access_belum_deny.py
@@ -48,9 +48,8 @@ for ip, count in counter.most_common():
             if ip_obj in net:
                 found = '✅ sudah di-deny'
                 break
-        # Tampilkan semua atau hanya yang belum di-deny, sesuai kebutuhan
-        # if found == '❌ belum di-deny':
-        print(f'https://ipinfo.io/{ip:<20}: {count:5} kali - {found}')
-        #print(f"{ip:<40} : {count:5} kali - {found}")
+        if found == '❌ belum di-deny':
+            print(f'https://ipinfo.io/{ip:<20}: {count:5} kali - {found}')
+            #print(f"{ip:<40} : {count:5} kali - {found}")
     except Exception:
         print(f'{ip}\t: {count:5} kali - ⚠ invalid IP')
